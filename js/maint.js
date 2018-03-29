ce108225d7bae9b131a335bcba6884f3c8454a25
Tom Booker <lovelocke@protonmail.com>
2018-03-29 15:05:59 +0100

    Second commit

--- js/maint.js
+++ js/maint.js
@@ -3,6 +3,29 @@
     @author Tom Booker
  */
 
+function checkMaint() {
+    $.getJSON('http://localhost:3000/getmaint', function (res) {
+        var caro = "<table class='striped centered'><thead><tr><th>Title</th><th>Msg</th><th>From</th><th>To</th></tr></thead><tbody>";
+
+        for (var i = 0; i < res.Data.length; i++) {
+
+            var datef = new Date(res.Data[i].datefrom);
+            var datet = new Date(res.Data[i].dateto);
+
+            /*caro += "<div id='"+ res.Data[i].id +"'><h5 class='alertTitle' id='"+ res.Data[i].id +".title' style='float: left;'>" +
+                res.Data[i].title + "</h5><p id='"+ res.Data[i].id +".text' style='float: left;'>" + res.Data[i].msg + "</p><h5 style='float: right;'>Date from:</h5><p style='float: right;'>" +
+                dateConv(datef) + "<h5 style='float: right;'>Date to:</h5><p style='float: right;'>" + dateConv(datet) + "</div>";*/
+
+            caro += "<tr id='"+ res.Data[i].id +"'><td><h5>" + res.Data[i].title + "</h5></td><td>" + res.Data[i].msg +
+                "</td><td>" + dateConv(datef) + "</td><td>" + dateConv(datet) + "</td></tr>";
+
+        }
+        caro += "</table>";
+        document.getElementById("maintHomeMsg").innerHTML = caro;
+
+    });
+}
+
 function checkMaintAdmin() {
     $.getJSON('http://localhost:3000/getmaint', function (res) {
         var caro = "";
